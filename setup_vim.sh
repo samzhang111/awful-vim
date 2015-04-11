a9463e20331bd3a8fa6e3647349d0c801a322457
samzhang111 <shimian.zhang@gmail.com>
2015-04-11 16:41:20 -0400

    Bundling vimrc here with tern shortcuts

--- setup_vim.sh
+++ setup_vim.sh
@@ -4,15 +4,18 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 mkdir -p ~/.vim/autoload ~/.vim/bundle 
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-curl "http://amix.dk/vim/vimrc.txt" > ~/.vimrc
-echo "execute pathogen#infect()" >> ~/.vimrc
+cp vimrc ~/.vimrc
 
 npm install -g bootlint
 npm install -g jshint
-npm install -g tern
 
 cd ~/.vim/bundle
 while read line
 do
     git clone $line
 done < "$DIR/plugins.txt"
+
+
+# Tern specific 
+cd ~/.vim/tern_for_vim
+npm install tern
